bfd870715afd44c49180b767a1e0fb88526b31a0
Jakub Jezek <jezschaj@gmail.com>
2018-07-07 13:52:02 +0200

    setting virtualenv

--- pipeline.bat
+++ pipeline.bat
@@ -1,46 +1,49 @@
+@echo off
 REM python27 = py2 ; python36 = py3
 set PYTHON=python
 set PYTHON_ENV=win\2.7.15
-set PIPELINE=%pipeline%\%PYTHON_ENV%
+set PY_PATH=%PYTHON%\%PYTHON_ENV%
 set MODULES_PY=py2
 set PIPLINE_ROOT=%~dp0
+set VENV_ROOT=1
+set python_exe=%PIPLINE_ROOT%%PY_PATH%\python.exe
 
-call python\environment_run.bat
+%python_exe% %PYTHON%\create_usr_virtualenv.py
 
-set ENV_ROOT=%VENV_ROOT%  
+set ENV_ROOT=%VENV_ROOT%
 
+echo %ENV_ROOT%
+echo This thing
+::set GIT_ROOT=%ENV_ROOT%pyblishdev\git\%MODULES_PY%
 
+::set PYBLISH_QML_PATH=%GIT_ROOT%/pyblish-qml
+::set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-nuke
+::set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-maya
+::set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-nukestudio
+::set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-integration
+::set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-houdiny
 
-set GIT_ROOT=%ENV_ROOT%pyblishdev\git\%MODULES_PY%
+::set QT5_PATH=%GIT_ROOT%/python-qt5
 
-set PYBLISH_QML_PATH=%GIT_ROOT%/pyblish-qml
-set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-nuke
-set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-maya
-set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-nukestudio
-set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-integration
-set PYBLISH_NUKE_PATH=%GIT_ROOT%/pyblish-houdiny
+::set PYTHONPATH=%ENV_ROOT%\Lib\site-packages;
 
-set QT5_PATH=%GIT_ROOT%/python-qt5
+::set QTCONF_PATH=%ENV_ROOT%\Scripts\qt.conf
 
-set PYTHONPATH=%ENV_ROOT%\Lib\site-packages;
+::cd /d %PYBLISHDEV_ROOT%
+:: call %GIT_DEPANDANCI%
+:: echo [92m^>^>^>[0m run depandancy check and install...
+:: set PYBLISH_QML_PYTHON_EXECUTABLE=%PYBLISHDEV_ROOT%venv\py2\Scripts\
+:: set PYBLISH_QML_PYQT5=%PYBLISHDEV_ROOT%git\py2\python-qt5
 
-set QTCONF_PATH=%ENV_ROOT%\Scripts\qt.conf
-
-cd /d %PYBLISHDEV_ROOT%
-REM call %GIT_DEPANDANCI%
-::echo [92m^>^>^>[0m run depandancy check and install...
-REM set PYBLISH_QML_PYTHON_EXECUTABLE=%PYBLISHDEV_ROOT%venv\py2\Scripts\
-REM set PYBLISH_QML_PYQT5=%PYBLISHDEV_ROOT%git\py2\python-qt5
-
-call %ENV_ROOT%\Scripts\activate.bat
+::call %ENV_ROOT%\Scripts\activate.bat
 :: will create qt.conf in Script folder in case there is none
-REM python -c "import util;util.createqtconf()"
-set PATH=%ENV_ROOT%\Scripts;%ENV_ROOT%
-cd %PYBLISHDEV_ROOT%
+:: python -c "import util;util.createqtconf()"
+::set PATH=%ENV_ROOT%\Scripts;%ENV_ROOT%
+::cd %PYBLISHDEV_ROOT%
 
 :: run pyblish from here
 
 
 
-::call %PYBLISHDEV_ROOT%.venv\%PYTHON_ENV%\Scripts\deactivate.bat
-::del %QTCONF_PATH%>nul 2>&1
+:: call %PYBLISHDEV_ROOT%.venv\%PYTHON_ENV%\Scripts\deactivate.bat
+:: del %QTCONF_PATH%>nul 2>&1
